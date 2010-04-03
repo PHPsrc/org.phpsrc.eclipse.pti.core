c0385c632854427acd8422b6deb3b32d2c6fa144
skiera <skiera@883fd64d-5c66-4666-9b8f-b541fcdf2f7e>
2010-04-03 08:33:51 +0000

    Closed 28: Switch to Eclipse Public Licence (EPL)

--- src/org/phpsrc/eclipse/pti/ui/wizards/fields/ComboStringDialogField.java
+++ src/org/phpsrc/eclipse/pti/ui/wizards/fields/ComboStringDialogField.java
@@ -1,14 +1,11 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2010 Sven Kiera
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *     Zend Technologies
  *******************************************************************************/
+
 package org.phpsrc.eclipse.pti.ui.wizards.fields;
 
 import org.eclipse.php.internal.ui.wizards.fields.StringDialogField;
