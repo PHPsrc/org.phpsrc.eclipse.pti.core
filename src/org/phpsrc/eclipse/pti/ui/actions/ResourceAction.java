0d637a6cd607814054c7028b5f7c91bd4b96b871
skiera <skiera@883fd64d-5c66-4666-9b8f-b541fcdf2f7e>
2010-05-16 07:12:03 +0000

    use editor input as default selection

--- src/org/phpsrc/eclipse/pti/ui/actions/ResourceAction.java
+++ src/org/phpsrc/eclipse/pti/ui/actions/ResourceAction.java
@@ -23,15 +23,16 @@ import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
-import org.eclipse.ui.PlatformUI;
 import org.phpsrc.eclipse.pti.core.PHPToolkitUtil;
 import org.phpsrc.eclipse.pti.ui.Logger;
 
 public abstract class ResourceAction implements IWorkbenchWindowActionDelegate {
+	private IWorkbenchWindow window;
 	private IResource[] selectedResources;
 
 	public void dispose() {
@@ -40,18 +41,15 @@ public abstract class ResourceAction implements IWorkbenchWindowActionDelegate {
 
 	public void init(IWorkbenchWindow window) {
 		selectedResources = new IResource[0];
+		this.window = window;
 	}
 
 	public void selectionChanged(IAction action, ISelection selection) {
 		ArrayList<IResource> resources = new ArrayList<IResource>(1);
-		if (selection instanceof ITextSelection) {
-			IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-			if (page != null && page.getActiveEditor() != null) {
-				IEditorInput input = page.getActiveEditor().getEditorInput();
-				if (input != null && input instanceof IFileEditorInput) {
-					addResourceToList(resources, ((IFileEditorInput) input).getFile());
-				}
-			}
+		if (selection.isEmpty()) {
+			addActiveEditorFileToList(resources);
+		} else if (selection instanceof ITextSelection) {
+			addActiveEditorFileToList(resources);
 		} else if (selection instanceof IStructuredSelection) {
 			IStructuredSelection structuredSelection = (IStructuredSelection) selection;
 			resources = new ArrayList<IResource>(structuredSelection.size());
@@ -89,6 +87,8 @@ public abstract class ResourceAction implements IWorkbenchWindowActionDelegate {
 					Logger.logException(e);
 				}
 			}
+		} else {
+			addActiveEditorFileToList(resources);
 		}
 
 		selectedResources = resources.toArray(new IResource[0]);
@@ -105,6 +105,19 @@ public abstract class ResourceAction implements IWorkbenchWindowActionDelegate {
 	private void addResourceToList(ArrayList<IResource> list, IResource resource) {
 		if (resource != null && resource.exists() && !list.contains(resource) && canAddResourceToList(resource))
 			list.add(resource);
+	}
+
+	protected void addActiveEditorFileToList(ArrayList<IResource> list) {
+		IWorkbenchPage page = window.getActivePage();
+		if (page != null) {
+			IEditorPart editor = page.getActiveEditor();
+			if (editor != null) {
+				IEditorInput input = editor.getEditorInput();
+				if (input != null && input instanceof IFileEditorInput) {
+					addResourceToList(list, ((IFileEditorInput) input).getFile());
+				}
+			}
+		}
 	}
 
 	protected boolean canAddResourceToList(IResource resource) {
